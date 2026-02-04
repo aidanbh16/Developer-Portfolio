921720d72d51e389499a67d4b4000ecd44457ce2
Aidan <aidanbh16@gmail.com>
2026-02-04 13:51:03 -0500

    CORS configured for production

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,12 @@
 import express from "express"
 import cors from "cors"
+import env from "./config"
 
 const app = express()
 
 app.use(
     cors({
-        origin: "http://localhost:3000"
+        origin: env.frontend_url
     })
 )
 
---
+++ backend/src/config.ts
@@ -0,0 +1,6 @@
+
+const env = {
+    frontend_url: process.env.PROD_DOMAIN ?? "http://localhost:3000"
+}
+
+export default env

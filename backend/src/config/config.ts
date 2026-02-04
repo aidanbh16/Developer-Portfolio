62f609664221d5ad56cc8fc60f4583331b759d91
Aidan <aidanbh16@gmail.com>
2026-02-04 16:35:05 -0500

    Fixed port

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -2,7 +2,7 @@ import "dotenv/config"
 
 const env = {
     frontend_url: process.env.FRONTEND_PROD_DOMAIN ?? "http://localhost:3000",
-    port: Number(process.env.PORT) ?? 8080,
+    port: Number(process.env.PORT) || 8080,
 }
 
 export default env
